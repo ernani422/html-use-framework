3524521a3e9b4a121f4bbe85d84c09b76d04075d
ERNANI DA PAZ <ernani.2990@aluno.pr.senac.br>
2022-10-04 22:35:29 -0300

    atualização

--- app.js
+++ app.js
@@ -12,7 +12,62 @@
                    document.getElementById('company-site').innerHTML = data.web_site_company
                    $("#show-values").css('display','block');
                });
+            }
+            function submitDataOfUser () {
+                const nameOfUser = $("#name").val()
+                const lasNameOfUser = $("#lastname").val()
+                const ageOfUser = $("#age").val()
 
+
+                console.log(nameOfUser)
+                console.log(lasNameOfUser)
+                console.log(ageOfUser)
+    
             
+            }
+            function closeSuccessMsg() {
+          
+                $("#success-record-msg").css('display', 'none')
+            
+          
+                 document.getElementById('success-record-msg').style.display = 'none'
+            } 
+        function submitDataOfUser() {
+                const nameOfUser = $("#name").val()
+                const lasNameOfUser = $("#lastname").val()
+                const ageOfUser = $("#age").val()      
 
-    }
+                $.ajax({
+                    "url": 
+                    "http://localhost:5000/insert-data",
+                    "method": "POST",
+                    "timeout": 0,
+                    "headers": {
+                      "Content-Type": "application/json; charset=UTF-8"
+                    },
+                    "data": JSON.stringify({
+                        name:nameOfUser,
+                        lastName:lasNameOfUser,
+                        age:ageOfUser
+                    }) 
+
+                }).done(function (response) {
+                    
+                    if (response.success) {
+                        $("#success-record-msg").css('display','block')
+            
+                        $("#name").val('')
+                        $("#lastname").val('')
+                        $("#age").val('')            
+                    } else {
+                        $("#error-record-msg").css('display','block')
+                        document.getElementById("error-record-msg").innerHTML += response.message;
+                    }
+            
+                });
+            } 
+
+
+        
+
+    
