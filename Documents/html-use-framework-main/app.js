06cd5471c9e1ab07e083f23e002b475908132d99
ERNANI DA PAZ <ernani.2990@aluno.pr.senac.br>
2022-10-22 12:26:40 -0300

    atualizar20/10

---
+++ Documents/html-use-framework-main/app.js
@@ -0,0 +1,84 @@
+let a =1
+let b =2*a
+
+function requestOfServer() {
+
+    $.ajax({
+        type: 'get',
+        url: "http://localhost:8000/hello-world?info=OlaMundo"
+    }).done(function(data) { 
+        document.getElementById('developer').innerHTML = data.name
+        document.getElementById('version').innerHTML = data.version
+        document.getElementById('value-of-variable').innerHTML = data.value_of_variable_info
+        document.getElementById('company-site').innerHTML = data.web_site_company
+
+        $("#show-values").css('display', 'block')
+
+    });
+
+}
+
+function submitDataOfUser() {
+    const nameOfUser = $("#name").val()
+    const lasNameOfUser = $("#lastname").val()
+    const ageOfUser = $("#age").val()      
+    $.ajax({
+        "url": "http://localhost:8000/insert-data",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json; charset=UTF-8"
+        },
+        "data": JSON.stringify({
+            name:nameOfUser,
+            lastname:lasNameOfUser,
+            age:ageOfUser
+        }) 
+    }).done(function (response) {
+        if (response.success) {
+            $("#success-record-msg").css('display','block')
+            $("#error-record-msg").css('display','none')
+
+            $("#name").val('')
+            $("#lastname").val('')
+            $("#age").val('')            
+        } else {
+            let errorMsg;
+            let errorWidth = '350px'
+
+            if (response.missingAttribute === 'name') {
+                errorMsg = 'O campo de nome está ausente, pode preencher'
+                errorWidth = '415px'
+            }
+
+            if (response.missingAttribute === 'lastname') {
+                errorMsg = 'O campo de sobrenome está ausente, pode preencher'
+                errorWidth = '435px'
+            }
+
+            if (response.missingAttribute === 'age') {
+                errorMsg = 'O campo de idade está ausente, pode preencher'
+                errorWidth = '415px'
+            }
+
+            $("#error-record-msg").css('display','block')
+            $("#success-record-msg").css('display','none')
+            $("#content-error-record-msg").html(errorMsg)
+            $("#error-record-msg").css('width',errorWidth)
+        }
+
+    });
+}
+
+
+function closeSuccessMsg() {
+    /* Jquery */
+    $("#success-record-msg").css('display', 'none')
+
+    /* vanila */
+    // document.getElementById('success-record-msg').style.display = 'none'
+}
+
+function closeErrorMsg() {
+    $("#error-record-msg").css('display', 'none')
+}
